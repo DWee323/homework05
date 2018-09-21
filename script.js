647cfab94d35981c38f8d24e47f1f09bd72b0b19
Diane Wee <hwee1@id.iit.edu>
2018-09-21 18:33:01 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+//Before commiting, delete all "commented out CODE"
+
+//2. Prompt the user once for an even number and save their answer to the variable user_input. Be sure to convert it to an integer.
+
+//this is just trusting the user's input to be an even number. If we're prompting only once, there is no choice but to.
+var user_input;
+user_input = prompt("Input an even number. An even number is 2k, where k is any positive integer.");
+user_input = parseInt(user_input, 10);
+
+
+
+
+//3. Using a do...while loop, keep asking the user for an even number until they enter an even number. At this point, there should be only one prompt() call in your code.
+
+//4. Prompt the user once for an odd number and save their answer to the variable user_input. Be sure to convert it to an integer.
+
+//5. Using a while loop, keep asking the user for an odd number until they enter an odd number. At this point, there should be only two prompt() call in your code; one for the even, and one for the odd.
+
+//6. Using a for loop, prompt the user 10 times to enter a fruit. Push each answer to a fruit array.
+
+//7. Prompt the user to enter a fruit to find in the array. Using a for loop and break, find the index of the fruit they are searching for. If it's in the array, output to the console the index of where that fruit is. Otherwise output to the console that it is not in the array.
+
+//8. Be sure to test your code before turning it in.
+
+console.log("메롱", user_input);
