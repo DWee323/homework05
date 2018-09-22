eef300042a41726f9351b9d80e4426e185a60098
Diane Wee <hwee1@id.iit.edu>
2018-09-22 14:45:26 -0500

    update: done_have to double check, but will push for now

--- script.js
+++ script.js
@@ -20,9 +20,32 @@ while (user_input % 2 == 0) {
 
 
 //6. Using a for loop, prompt the user 10 times to enter a fruit. Push each answer to a fruit array.
+var fruits = [];
+var fruit_input;
+
+for (let i = 0 ; i < 10 ; i++){
+    fruit_input = prompt("Enter a fruit name. You will be asked to do this " + (9-i) + " more times after this input.");
+    fruits.push(fruit_input);    
+};
+//console.log(fruits, fruits.length);
 
 //7. Prompt the user to enter a fruit to find in the array. Using a for loop and break, find the index of the fruit they are searching for. If it's in the array, output to the console the index of where that fruit is. Otherwise output to the console that it is not in the array.
 
+var to_find = prompt("Search for a fruit name.");
+//which index is cherries? oranges? 이걸 알고싶을때(search)
+// == small case upper case 일치해야함 !
+for (var fruit_i = 0; fruit_i <fruits.length; fruit_i++){
+    if (fruits[fruit_i] == to_find){
+        break;
+    }
+};
+
+if (fruit_i == fruits.length){
+    console.log("'" + to_find + "' is not found in the fruits array.")
+} else {
+    console.log("'" + to_find + "' is found at index " + fruit_i + " in the fruits array.");
+};
+
 //8. Be sure to test your code before turning it in.
 
-console.log("메롱", user_input);
+console.log("메롱", user_input, fruits, fruits.length);
