a4ae4b1812349873766f615ad722f64be09d7f56
Diane Wee <hwee1@id.iit.edu>
2018-09-22 13:28:34 -0500

    update: YAS! #2,3 part finally worked

--- script.js
+++ script.js
@@ -1,41 +1,15 @@
-//Just declaring a var, not yet assigning a particular value. i.e. "let user_input be nothing".
-/*
-var user_Input;
-user_Input = prompt("Input an even number. An even number is 2k, where k is any positive integer or 0.");
-if ( parseInt(user_Input, 10)%2 == 0 ) {
-    user_Input = parseInt(user_Input, 10);
-    alert("Awesome, " + user_Input + " is an even number!");
-} else {
-    alert("Eh, " + user_Input + " is not an even number.");    
-};
-*/
-
-
-
-
 //2. Prompt the user once for an even number and save their answer to the variable user_input. Be sure to convert it to an integer.
 
 //3. Using a do...while loop, keep asking the user for an even number until they enter an even number. At this point, there should be only one prompt() call in your code.
 
-//WORKS! FINALLY!
-/*
+//Just declaring a var, not yet assigning a particular value. i.e."let user_input be nothing".
 var user_input;
-user_input = prompt("enter an even number. And even number is 2k, where k is any positive integer or zero.");
-user_input = parseInt(user_input, 10);
-console.log(user_input);
-*/
 
-var user_input;
 do {
     user_input = prompt("Enter an even number. And even number is 2k, where k is any positive integer or zero.");
-    user_input = praseInt(user_input, 10);
+    user_input = parseInt(user_input, 10);
 } while ( user_input % 2 != 0 );
 
-let anSwer; 
-do{
-    anSwer = prompt("Enter 10. Or pop-up will keep asking.");
-    anSwer = parseInt(anSwer, 10); 
-} while(anSwer != 10);
 
 
 
@@ -58,4 +32,4 @@ do{
 
 //8. Be sure to test your code before turning it in.
 
-console.log("메롱", user_Input);
+console.log("메롱", user_input);
