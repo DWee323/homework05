46da01f048fd0b961ca1c60be6186bcc8ef072de
Diane Wee <hwee1@id.iit.edu>
2018-09-22 14:17:47 -0500

    update: fixed k, up to #5 work

--- script.js
+++ script.js
@@ -1,30 +1,23 @@
 //2. Prompt the user once for an even number and save their answer to the variable user_input. Be sure to convert it to an integer.
-
 //3. Using a do...while loop, keep asking the user for an even number until they enter an even number. At this point, there should be only one prompt() call in your code.
 
 //Just declaring a var, not yet assigning a particular value. i.e."let user_input be nothing".
 var user_input;
 
 do {
-    user_input = prompt("Enter an even number. And even number is 2k, where k is any positive integer or zero.");
+    user_input = prompt("Enter an even number. An even number is 2k, where k is any integer.");
     user_input = parseInt(user_input, 10);
 } while ( user_input % 2 != 0 );
-
-
-
-
-/*
-do{
-    unser_input = prompt("Input an even number. An even number is 2k, where k is any positive integer or 0.");
-    unser_input = parseInt(unser_input, 10); 
-} while(parseInt(user_input, 10)%2 != 0);
-*/
-
-
+                          
 
 //4. Prompt the user once for an odd number and save their answer to the variable user_input. Be sure to convert it to an integer.
-
 //5. Using a while loop, keep asking the user for an odd number until they enter an odd number. At this point, there should be only two prompt() call in your code; one for the even, and one for the odd.
+
+while (user_input % 2 == 0) {
+    user_input = prompt("Enter an odd number. An odd number is 2k+1, where k is any integer.");
+    user_input = parseInt(user_input, 10);
+}
+
 
 //6. Using a for loop, prompt the user 10 times to enter a fruit. Push each answer to a fruit array.
 
